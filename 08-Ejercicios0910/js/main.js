47c6c3b0f1c4d32461caf3a55d648b44f4d495d9
Oscar Galvez <oscar.galvez27@outlook.com>
2021-09-13 18:34:40 -0500

    0913

---
+++ 08-Ejercicios0910/js/main.js
@@ -0,0 +1,12 @@
+// 1. Dado un string de palabras, devolver la palabra más larga.
+
+//let message = prompt("Enter your message here:");
+//console.log(message.length);
+
+//2.Crear un programa que permita detectar si una frase finaliza con punto.
+//let puntoMessage = prompt("Enter your message here:");
+//puntoMessage = /[.]/gi;
+
+function pyramyd() {
+  let rows = 10;
+}
