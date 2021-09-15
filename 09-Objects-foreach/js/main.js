b40c20236eb5c3581a4a30359b84f551d9e5226c
Oscar Galvez <oscar.galvez27@outlook.com>
2021-09-14 21:25:55 -0500

    Ejercicio tarea

--- 09-Objects-foreach/js/main.js
+++ 09-Objects-foreach/js/main.js
@@ -102,7 +102,7 @@ console.log(myColor2);
 console.log(myColor3);
 */
 
-let dataarray = [
+let dataArray = [
   ["Fernanda", "Palacios"],
   ["Alfred", "Altamirano"],
   ["Angel", "Resendiz"],
@@ -122,3 +122,241 @@ const getArrayWithObject = (arrayMentors) => {
   }
   return newArrayMentors;
 };
+
+console.log("**********************************************");
+
+let otherArrayMentors = getArrayWithObject(dataArray);
+
+//console.log(otherArrayMentors);
+
+let namesArray = ["Elias", "Pao", "Pablo", "Nancy"];
+
+namesArray.forEach((name, index) => {
+  console.log(`el nombre es: ${name} en la posicion ${index}`);
+});
+
+console.log("**********************************************");
+
+let arrayMentors = [
+  {
+    name: "Fernanda",
+    lastName: "Palacios",
+  },
+  {
+    name: "Alfred",
+    lastName: "Altamirano",
+  },
+];
+
+arrayMentors.forEach((mentor, index) => {
+  console.log(mentor.name);
+  console.log(index);
+});
+
+let mentorArray = [
+  {
+    name: "Angel Resendiz",
+    scores: [
+      {
+        signature: "HTML",
+        score: 10,
+      },
+      {
+        signature: "CSS",
+        score: 10,
+      },
+      {
+        signature: "JS",
+        score: 9,
+      },
+    ],
+  },
+  {
+    name: "Elda Corona",
+    scores: [
+      {
+        signature: "HTML",
+        score: 10,
+      },
+      {
+        signature: "CSS",
+        score: 9,
+      },
+      {
+        signature: "JS",
+        score: 10,
+      },
+    ],
+  },
+  {
+    name: "Alfred Altamirando",
+    scores: [
+      {
+        signature: "HTML",
+        score: 9,
+      },
+      {
+        signature: "CSS",
+        score: 10,
+      },
+      {
+        signature: "JS",
+        score: 10,
+      },
+    ],
+  },
+  {
+    name: "Tux Tuxtla",
+    scores: [
+      {
+        signature: "HTML",
+        score: 10,
+      },
+      {
+        signature: "CSS",
+        score: 8,
+      },
+      {
+        signature: "JS",
+        score: 10,
+      },
+    ],
+  },
+  {
+    name: "Fernanda Palacios",
+    scores: [
+      {
+        signature: "HTML",
+        score: 10,
+      },
+      {
+        signature: "CSS",
+        score: 9,
+      },
+      {
+        signature: "JS",
+        score: 10,
+      },
+    ],
+  },
+];
+
+/*
+- Obtener el score promedio de cada materia (HTML, CSS, JS)
+- Obtener el promedio indivual de cada mentor
+- Crear un array de string con la siguiente forma:
+  "Mi nombre es {name} y mi promedio es {promedio}"
+- Obtener la lista de mentores cuyo promedio sea mayor a 9.5
+*/
+
+/*
+- Obtener el score promedio de cada materia (HTML, CSS, JS)
+html
+css
+js
+*/
+
+const getAverageAsignatures = (arrayMentors) => {
+  let htmlAccum = 0;
+  let cssAccum = 0;
+  let jsAccum = 0;
+  let mentorsQty = arrayMentors.length;
+  arrayMentors.forEach((mentorObject, index) => {
+    // console.log(mentorObject)
+    mentorObject.scores.forEach((scoreMentor) => {
+      // console.log(scoreMentor)
+      let signature = scoreMentor.signature;
+      let score = scoreMentor.score;
+
+      switch (signature) {
+        case "HTML":
+          htmlAccum += score;
+          break;
+        case "CSS":
+          cssAccum += score;
+          break;
+        case "JS":
+          jsAccum += score;
+          break;
+        default:
+          console.log("Asignatura no encontrada");
+      }
+    });
+  });
+
+  let avgHtml = htmlAccum / mentorsQty;
+  let avgCss = cssAccum / mentorsQty;
+  let avgJs = jsAccum / mentorsQty;
+
+  let objectAvg = { avgHtml, avgCss, avgJs };
+  return objectAvg;
+};
+
+//invocar
+let avgSignatures = getAverageAsignatures(mentorsArray);
+console.log(avgSignatures);
+
+/*
+- Obtener el promedio indivual de cada mentor
+*/
+
+const getAvgForMentor = (scoresArray) => {
+  let accumMentor = 0;
+  let scoresQty = scoresArray.length;
+  scoresArray.forEach((scoreMentor) => {
+    let valueScore = scoreMentor.score;
+    accumMentor += valueScore;
+  });
+  let avgMentor = accumMentor / scoresQty;
+  return avgMentor;
+};
+
+/*
+arrayAverage = [
+  { 
+      name: "",
+      average: 9.0
+  }
+]
+*/
+
+const getAvgMentors = (arrayMentors) => {
+  let arrayAverageMentors = [];
+  arrayMentors.forEach((mentor) => {
+    let avg = getAvgForMentor(mentor.scores);
+    let objectMentor = {
+      name: mentor.name,
+      average: avg,
+    };
+    arrayAverageMentors.push(objectMentor);
+  });
+  return arrayAverageMentors;
+};
+
+let avgMentors = getAvgMentors(mentorsArray);
+
+console.log(avgMentors);
+
+/*
+- Crear un array de string con la siguiente forma:
+"Mi nombre es {name} y mi promedio es {promedio}"
+*/
+
+let arrayStringsMentors = [];
+
+avgMentors.forEach((mentor) => {
+  // destructuring
+  let { name, average } = mentor;
+  arrayStringsMentors.push(`Mi nombre es ${name} y mi promedio es ${average}`);
+});
+
+console.log(arrayStringsMentors);
+
+// - Obtener la lista de mentores cuyo promedio sea mayor a 9.5
+
+let mentorsTop = [];
+
+avgMentors.forEach((mentor) => {
+  mentor.average >= 9.5 ? mentorsTop.push(mentor) : null;
+});
+console.log(mentorsTop);
